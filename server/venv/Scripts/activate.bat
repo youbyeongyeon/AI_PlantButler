e640584df34b978a8541c4e2603b4a5271ea56d3
youbyeongyeon <youby020919@gmail.com>
2025-08-26 09:42:08 +0900

    디렉토리 정리

--- server/venv/Scripts/activate.bat
+++ server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\youby\Documents\GitHub\AI_PlantButler\server\venv
+set VIRTUAL_ENV=C:\AI_PlantButler\AI_PlantButler\server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
